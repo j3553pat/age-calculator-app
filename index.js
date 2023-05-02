80aaafb5d41f1bb82ee157745869bdee2f840cf7
j3553pat <jessecodedev@gmail.com>
2023-05-02 16:49:40 -0500

    complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btnEl = document.getElementById("btn")
 const birthdayEl = document.getElementById("birthday")
+const resultEl = document.getElementById("result")
 
 const ageCalculate = () => {
     const birthdayValue = birthdayEl.value;
@@ -7,12 +8,22 @@ const ageCalculate = () => {
         alert("Please enter valid date.")
     } else {
         const age = getAge(birthdayValue)
+        resultEl.innerText = `You are ${age} ${age > 1 ? "years": "year"} old!`
     }
 
 }
 
 const getAge = (birthdayValue) => {
     const currentDate = new Date()
+    const birthdayDate = new Date(birthdayValue)
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear()
+    const month = currentDate.getMonth() - birthdayDate.getMonth()
+
+    if(month < 0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate())) {
+        age--
+    }
+
+    return age
 }
 
 btn.addEventListener("click", ageCalculate)
